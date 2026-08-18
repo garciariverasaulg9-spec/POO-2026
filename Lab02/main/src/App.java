944ddcd5f3cd358c9d3e53cee6e8227b4203e3cd
Saul <garciariverasaulg9@gmail.com>
2026-08-18 10:14:41 -0600

    LAB02

--- Lab02/main/src/App.java
+++ Lab02/main/src/App.java
@@ -1,13 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Perro miPerro= new Perro("Wera",6,10.4f,"Criolla",true);
-        Canario miAve= new Canario("jose", 2,10.2f,"Azul",true,10);
+        Canario miAve= new Canario("Pico", 2,0.4f,"Azul",true,10);
         Gato miGato= new Gato("Michi",67,11.4f,"Siames",true,"Naranja");
+        System.out.println("--Perro--");
+        System.out.println(miPerro.toString());
         miPerro.buscarPelota();
-        System.out.println(miPerro.toString()+"\n");
+        miPerro.dormir();
+        System.out.println("\n");
+
+        System.out.println("--Canario--");
+        System.out.println(miAve.toString());
         miAve.volar();
-        System.out.println(miAve.toString()+"\n");
-        miGato.maullar();
+        miAve.dormir();
+        miAve.cantar();
+        miAve.picar();
+        System.out.println("\n");
+
+
+        System.out.println("--Gato--");
         System.out.println(miGato.toString());
+        miGato.maullar();
+        miGato.dormir();
+        miGato.jugar();
+        miGato.aranar();
+        System.out.println("\n");
     }
 }
